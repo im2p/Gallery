760442806c4ae69c84b72257b3d253d6f6c45c68
im2p <nicholastupy@gmail.com>
2015-05-15 16:40:26 -0500

    Started dynamic nav

--- ajax.php
+++ ajax.php
@@ -2,6 +2,8 @@
         $filearray = array();
         $imgtitlearray = array();
 		$dimensionsarray = array();
+		$folderarray = array();
+		$foldernames = array();
 
         foreach (glob("img/port/*.jpg") as $file) {
             $filearray[] = $file;
@@ -16,7 +18,18 @@
             $thumbarray[] = $thumbpath;
         }
 
+		foreach (glob("img/*", GLOB_ONLYDIR) as $foldername) {
+			$folderarray[] = $foldername;
+		}
+		foreach (glob("img/*", GLOB_ONLYDIR) as $fnames) {
+            $foldernames[] = basename($fnames);
+            $fnames = preg_replace('/\\.[^.\\s]{3,4}$/', '', $foldernames);
+        }
 //        echo json_encode($filearray);
-        echo json_encode(array('imgpath'=>$filearray,'imgtitle'=>$imgtitle,'dimensions'=>$dimensionsarray,'thumbarray'=>$thumbarray));
-
+        echo json_encode(array('imgpath'=>$filearray,
+							   'imgtitle'=>$imgtitle,
+							   'dimensions'=>$dimensionsarray,
+							   'folderpath'=>$folderarray,
+							   'foldername'=>$foldernames,
+							   'thumbarray'=>$thumbarray));
 ?>
--- pages/2010.php
+++ pages/2010.php
@@ -8,5 +8,18 @@
 		<div id="contentdiv">
 			2010
 		</div>
+		<script>
+			var i = 0;
+			$('#contentdiv').click(function () {
+				i+=1;
+				$(this).append('<br>'+ i);
+				if (i % 3 === 0) {
+					$(this).append('fizz');
+				}
+				if (i % 5 === 0) {
+					$(this).append('buzz');
+				}
+			});
+		</script>
 	</body>
 </html>
